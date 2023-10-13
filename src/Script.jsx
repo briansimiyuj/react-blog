effc07e82c86c92310d5f420fca5c70b79755102
Brian Simiyu <briansimiyuj@gmail.com>
2023-10-13 12:26:37 +0300

    Creating components

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,3 +1,12 @@
+import Header from './Header'
+import Nav from './Nav'
+import Footer from './Footer'
+import Home from './Home'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import About from './About'
+import Missing from './Missing'
+
 function Script() {
   return (
     <div className="script">
@@ -5,7 +14,7 @@ function Script() {
       
 
     </div>
-  );
+  )
 }
 
-export default Script;
+export default Script
