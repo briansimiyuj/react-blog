e33bd16eaa783a25d3c7a58f6ba37bc78f2c4c38
Brian Simiyu <briansimiyuj@gmail.com>
2023-10-13 12:14:16 +0300

    Installing react router

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css'
 import Script from './Script'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Script />
+    
+    <Router>
+
+      <Route path="/" component={Script}></Route>
+
+    </Router>
+
   </React.StrictMode>
 );
