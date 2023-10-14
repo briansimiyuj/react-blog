f0eed59d822a740cb60b378a969e1827fc550d16
Brian Simiyu <briansimiyuj@gmail.com>
2023-10-14 08:45:42 +0300

    Nav list markup

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 const Nav = ({ search, setSearch }) =>{
 
     return(
@@ -15,6 +17,17 @@ const Nav = ({ search, setSearch }) =>{
                     value={search}
                     onChange={e => setSearch(e.target.value)}
                 />
+
+
+                <ul>
+
+                    <li><Link to="/">Home</Link></li>
+
+                    <li><Link to="/post">Post</Link></li>
+                    
+                    <li><Link to="/about">About</Link></li>
+
+                </ul>
 
            </form>
 
