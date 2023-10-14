ce9be863c7676b0c73260edf6d4edb5f82afb25a
Brian Simiyu <briansimiyuj@gmail.com>
2023-10-14 08:58:28 +0300

    Nav content

--- src/Script.jsx
+++ src/Script.jsx
@@ -10,12 +10,49 @@ import { Route, Switch, useHistory } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 
 function Script() {
+
+  const [search, setSearch] = useState(''),
+
+        [results, setResults] = useState([]),
+
+        [posts, setPosts] = useState([
+
+          {
+            id: 1,
+            title: "My First Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+          },
+          
+          {
+            id: 2,
+            title: "My 2nd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+          },
+          
+          {
+            id: 3,
+            title: "My 3rd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+          },
+          
+          {
+            id: 4,
+            title: "My Fourth Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+          }
+
+        ])
+
   return (
     <div className="script">
       
       <Header title="React JS Blog"/>
 
-      <Nav/>
+      <Nav search={search} setSearch={setResults}/>
 
       <Switch>
 
