991b0b8c11fd6274d319aefe3d26674c95c90274
Brian Simiyu <briansimiyuj@gmail.com>
2023-10-19 12:35:01 +0300

    DataContext markup

--- src/Script.jsx
+++ src/Script.jsx
@@ -7,201 +7,45 @@ import PostPage from './PostPage'
 import About from './About'
 import Missing from './Missing'
 import EditPost from './EditPost'
-import { Route, Switch, useHistory } from 'react-router-dom'
-import { useState, useEffect } from 'react';
-import { format } from 'date-fns';
-import API from './api/posts';
-import useWindowSize from './hooks/useWindowSize';
-import useAxiosFetch from './hooks/useAxiosFetch';
+import DataProvider from './context/DataContext';
+import { Switch, Route } from 'react-router-dom'
 
 function Script() {
 
-  const [search, setSearch] = useState(''),
-
-        [results, setResults] = useState([]),
-
-        [postTitle, setPostTitle] = useState(''),
-
-        [postBody, setPostBody] = useState(''),
-
-        [editTitle, setEditTitle] = useState(''),
-
-        [editBody, setEditBody] = useState(''),
-
-        [posts, setPosts] = useState([]),
-
-        { width } = useWindowSize(),
-
-        { data, fetchError, isLoading } = useAxiosFetch('http://localhost:8000/posts'),
-
-        history = useHistory()
-
-
-    useEffect(() =>{
-    
-       setPosts(data)
-     
-    }, [data]) 
-    
-
-
-    useEffect(() =>{
-    
-      const filteredResults = posts.filter(post => 
-        
-        ((post.body).toLowerCase()).includes(search.toLowerCase())
-
-        ||  ((post.title).toLowerCase()).includes(search.toLowerCase())
-        
-      )
-
-      setResults(filteredResults.reverse())
-    
-    }, [posts, search])
-
-
-    const handleEdit = async (id) =>{
-    
-      const datetime = format(new Date(), 'MMM dd, yyyy pp'),
-
-            updatedPost = { id, title: editTitle, body: editBody, datetime }
-
-
-      try{
-
-        const response = await API.put(`/posts/${id}`, updatedPost)
-
-        setPosts(posts.map(post => post.id === id ? { ...response.data } : post))
-
-        setEditTitle('')
-
-        setEditBody('')
-
-        history.push('/')
-
-      }catch(err){
-
-        console.log(`Error: ${err.message}`)
-
-      }
-    
-    }
-
-
-  const handleDelete = async (id) =>{
-  
-      try{
-
-        await API.delete(`/posts/${id}`)
-
-        const postList = posts.filter(post => post.id !== id)
-
-        setPosts(postList)
-
-        history.push('/')
-
-      }catch(err){
-
-        console.log(`Error: ${err.message}`)
-
-      }
-  
-  }
-
-
-  const handleSubmit = async (e) =>{
-  
-     e.preventDefault()
-
-     const id = posts.length ? posts[posts.length - 1].id + 1 : 1,
-
-          datetime = format(new Date(), 'MMM dd, yyyy pp'),
-
-          newPost = { id, title: postTitle, body: postBody, datetime }
-
-      try{
-
-        const response = await API.post('/posts', newPost),
-
-             allPosts = [...posts, response.data]
-      
-        setPosts(allPosts)
-
-        setPostTitle('')
-
-        setPostBody('')
-
-        history.push('/')
-
-      }catch(err){}
-  
-  }
-
-
   return (
-    <div className="script">
-      
-      <Header title="React JS Blog" width={width}/>
+    <div className="script">      
 
-      <Nav search={search} setSearch={setSearch}/>
+      <Header title="React JS Blog"/>
 
-      <Switch>
+      <DataProvider>
 
-        <Route exact path="/">
+        <Nav search={search} setSearch={setSearch}/>
 
-          <Home 
-            posts={results}
-            fetchError={fetchError}
-            isLoading={isLoading}
-          />
+        <Switch>
 
-        </Route>
+          <Route exact path="/" component={Home}/>
 
 
-        <Route exact path="/post">
-
-          <NewPost
-            handleSubmit={handleSubmit}
-            postTitle={postTitle}
-            setPostTitle={setPostTitle}
-            postBody={postBody}
-            setPostBody={setPostBody}
-          />
-
-        </Route>
-        
-        
-        <Route path="/edit/:id">
-
-          <EditPost
-            posts={posts}
-            handleEdit={handleEdit}
-            editTitle={editTitle}
-            setEditTitle={setEditTitle}
-            editBody={editBody}
-            setEditBody={setEditBody}
-          />
-
-        </Route>
+          <Route exact path="/post" component={NewPost}/>
+          
+          
+          <Route path="/edit/:id" component={EditPost}/>
 
 
-        <Route exact path="/post/:id">
-
-          <PostPage posts={posts} handleDelete={handleDelete}/>
-
-        </Route>
+          <Route exact path="/post/:id" component={PostPage}/>
 
 
-        <Route path="/about" component={About}/>
+          <Route path="/about" component={About}/>
 
-        
-        <Route path="*" component={Missing}/>
+          
+          <Route path="*" component={Missing}/>
 
-      </Switch>
+        </Switch>
 
-      <Footer/>
+      </DataProvider>
 
-      
+        <Footer/>
+
 
     </div>
   )
---
+++ src/context/DataContext.jsx
@@ -0,0 +1,151 @@
+import { createContext, useState, useEffect } from 'react'
+import {  useHistory } from 'react-router-dom'
+import { format } from 'date-fns';
+import API from '../api/posts';
+import useWindowSize from '../hooks/useWindowSize';
+import useAxiosFetch from '../hooks/useAxiosFetch';
+
+const DataContext = createContext({})
+
+export const DataProvider = ({ children }) =>{
+
+    const [search, setSearch] = useState(''),
+
+        [results, setResults] = useState([]),
+
+        [postTitle, setPostTitle] = useState(''),
+
+        [postBody, setPostBody] = useState(''),
+
+        [editTitle, setEditTitle] = useState(''),
+
+        [editBody, setEditBody] = useState(''),
+
+        [posts, setPosts] = useState([]),
+
+        { width } = useWindowSize(),
+
+        { data, fetchError, isLoading } = useAxiosFetch('http://localhost:8000/posts'),
+
+        history = useHistory()
+
+
+    useEffect(() =>{
+    
+       setPosts(data)
+     
+    }, [data]) 
+    
+
+
+    useEffect(() =>{
+    
+      const filteredResults = posts.filter(post => 
+        
+        ((post.body).toLowerCase()).includes(search.toLowerCase())
+
+        ||  ((post.title).toLowerCase()).includes(search.toLowerCase())
+        
+      )
+
+      setResults(filteredResults.reverse())
+    
+    }, [posts, search])
+
+
+    const handleEdit = async (id) =>{
+    
+        const datetime = format(new Date(), 'MMM dd, yyyy pp'),
+  
+              updatedPost = { id, title: editTitle, body: editBody, datetime }
+  
+  
+        try{
+  
+          const response = await API.put(`/posts/${id}`, updatedPost)
+  
+          setPosts(posts.map(post => post.id === id ? { ...response.data } : post))
+  
+          setEditTitle('')
+  
+          setEditBody('')
+  
+          history.push('/')
+  
+        }catch(err){
+  
+          console.log(`Error: ${err.message}`)
+  
+        }
+      
+      }
+  
+  
+    const handleDelete = async (id) =>{
+    
+        try{
+  
+          await API.delete(`/posts/${id}`)
+  
+          const postList = posts.filter(post => post.id !== id)
+  
+          setPosts(postList)
+  
+          history.push('/')
+  
+        }catch(err){
+  
+          console.log(`Error: ${err.message}`)
+  
+        }
+    
+    }
+  
+  
+    const handleSubmit = async (e) =>{
+    
+       e.preventDefault()
+  
+       const id = posts.length ? posts[posts.length - 1].id + 1 : 1,
+  
+            datetime = format(new Date(), 'MMM dd, yyyy pp'),
+  
+            newPost = { id, title: postTitle, body: postBody, datetime }
+  
+        try{
+  
+          const response = await API.post('/posts', newPost),
+  
+               allPosts = [...posts, response.data]
+        
+          setPosts(allPosts)
+  
+          setPostTitle('')
+  
+          setPostBody('')
+  
+          history.push('/')
+  
+        }catch(err){
+
+          console.log(err)
+
+        }
+    
+    }
+
+    return(
+
+        <DataContext.Provider value={{
+
+        }}>
+
+            {children}
+
+        </DataContext.Provider>
+
+    )
+
+}
+
+export default DataContext
